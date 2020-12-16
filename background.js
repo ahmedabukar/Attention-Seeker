8d181e5bb6f6d514a194a0dc335bb3076fc6aa79
Ahmed Abukar <ahmedabukar@Ahmeds-MacBook-Pro.local>
2020-12-15 19:36:57 -0500

    whitelist

--- background.js
+++ background.js
@@ -1,8 +1,16 @@
 // Fires when a request is about to occur.
 // This event is sent before any TCP connection is made.
 
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) { return {cancel: true}; },
-  {urls: ["*://www.bing.com/*"]},
-  ["blocking"]
-);
+
+// Uncomment to use
+window.onload = function(){
+
+  var allowed = ["bing.com/", "nbclearn.com/", "google.com/"];
+
+  chrome.webRequest.onBeforeRequest.addListener(function(details) {
+      var isForbidden = allowed.every(function(url) {
+          return details.url.indexOf(url) == -1;
+      });
+      return {cancel: isForbidden}
+  }, {urls: ["<all_urls>"]}, ["blocking"]);
+};
