b05edf5f3084516c62d9a9beefe009bdda283797
Ahmed Abukar <ahmedabukar@Ahmeds-MacBook-Pro.local>
2020-12-17 17:00:10 -0500

    form data handling

--- background.js
+++ background.js
@@ -2,15 +2,16 @@
 // This event is sent before any TCP connection is made.
 
 
-// Uncomment to use
-// window.onload = function(){
+//  Uncomment to use
+window.onload = function(){
 
-//   var allowed = ["bing.com/", "google.com/"];
+  var allowed = ["bing.com/", "google.com/", "cckjogafdjpgbeelcigdikljjmhmbbop"];
 
-//   chrome.webRequest.onBeforeRequest.addListener(function(details) {
-//       var isForbidden = allowed.every(function(url) {
-//           return details.url.indexOf(url) == -1;
-//       });
-//       return {cancel: isForbidden}
-//   }, {urls: ["<all_urls>"]}, ["blocking"]);
-// }; 
+  chrome.webRequest.onBeforeRequest.addListener(function(details) {
+      var isForbidden = allowed.every(function(url) {
+          return details.url.indexOf(url) == -1;
+      });
+      return {cancel: isForbidden}
+  }, {urls: ["<all_urls>"]}, ["blocking"]);
+}; 
+
--- popup.js
+++ popup.js
@@ -1,21 +1,48 @@
 
-// test for events
+
+
+// Ideally this shift would be given to us as input by the instructor
+let shift = 2;
+ // test for events
+
 document.getElementById("userData").addEventListener("submit", myFunction);
 
+
 function myFunction() {
+
+  // Gets syudent name from form field
+  var stuName = document.getElementById("sname").value;
+
   alert("The form was submitted");
-  console.log("This is the start");
-  // get user input from form
-  var name = document.getElementById('email');
-  var email = document.getElementById('name');
-  var time = document.getElementById('time');
-  var websites = document.getElementById('websites');
+  sleep(5000);
+  // encyrpt name
+  // student can submit this key as a canvas assignment and canvas answer key will decrypt it and give the student
+  // points if the decrypted message equals the student's name
+  encrypt(stuName);
+  }
+  function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+  }
+  // ideally this would be RSA but for now we will implement Caesar's Cipher
+  function encrypt(stuName){
   
-  // print stuff
-  console.log(name);
-  console.log(email);
-  console.log(time);
-  console.log(websites);
-  console.log("The input ends here");
+  
+  // creates empty string to append the shifted characters to
+  var res = "";
+  stuName = stuName.toLowerCase();
+  // go through each character and shift it's ascii value and return the corresponding character
+  for (i = 0; i < stuName.length; i++) {
+    val = stuName.charCodeAt(i) + shift;
+    // in the event that the value goes beyond the number of ascii characters
+    if(val > 255){
+      val = val%255;
+    }
+    res += String.fromCharCode(val);
+  }
+  alert("Encrypted name: " +res);
 }
 
